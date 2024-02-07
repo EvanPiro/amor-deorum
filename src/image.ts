04ed8eaa7094357628d8ade67c6787962d4d2d0d
EvanPiro <dev.evanpiro@gmail.com>
2024-02-07 13:16:22 -0500

    latest

--- src/image.ts
+++ src/image.ts
@@ -1,37 +1,35 @@
-import { TaskEither } from "fp-ts/TaskEither";
-import * as te from "fp-ts/TaskEither";
+import { TaskEither } from 'fp-ts/TaskEither';
+import * as te from 'fp-ts/TaskEither';
 
 export const genImage =
-  (OPENAI_API_KEY: string) =>
-  (prompt: string): TaskEither<any, any> =>
-    te.tryCatch(
-      async () => {
-        console.log("gen image started with prompt", prompt);
-        const response = await fetch(
-          "https://api.openai.com/v1/images/generations",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${OPENAI_API_KEY}`,
-            },
-            body: JSON.stringify({
-              model: "dall-e-3",
-              prompt,
-              n: 1,
-              size: "1024x1024",
-            }),
-          }
-        );
+	(OPENAI_API_KEY: string) =>
+	(prompt: string): TaskEither<any, any> =>
+		te.tryCatch(
+			async () => {
+				console.log('gen image started with prompt', prompt);
+				const response = await fetch('https://api.openai.com/v1/images/generations', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${OPENAI_API_KEY}`,
+					},
+					body: JSON.stringify({
+						model: 'dall-e-3',
+						prompt,
+						n: 1,
+						size: '1024x1024',
+					}),
+				});
 
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
+				if (!response.ok) {
+					console.log(Error, await response.json());
+					throw new Error(`Error: ${response.status}`);
+				}
 
-        const data = await response.json();
+				const data = await response.json();
 
-        console.log(data);
-        return data.data[0].url;
-      },
-      (err: any) => err
-    );
+				console.log(data);
+				return data.data[0].url;
+			},
+			(err: any) => err
+		);
--- src/index.ts
+++ src/index.ts
@@ -18,25 +18,35 @@ export interface Env {
 	TWITTER_BEARER_TOKEN: string;
 }
 
+// const works = [
+// 	'The Gospels of the New Testament of the King James Bible',
+// 	'The Book of Job',
+// 	'The Book of Numbers',
+// 	'The Book of Deuteronomy',
+// 	'The Book of Revelations',
+// 	'The Tibetan Book of the Dead',
+// 	"Edward Gibbon's The History of the Decline and Fall of the Roman Empire Book 3 and 4",
+// 	'The Poetic Edda',
+// 	"Herodotus's The Histories",
+// 	'Secret History of the Mongols',
+// 	"Ovid's Metamorphoses",
+// 	"Virgil's Aeneid",
+// 	'The Epic of Gilgamesh',
+// 	'The Mahabharata',
+// 	"Sima Qian's Records of the Grand Historian",
+// 	'Beowulf',
+// 	"Homer's Odyssey",
+// 	"Homer's Iliad",
+// ];
+
 const works = [
-	'The Gospels of the New Testament of the King James Bible',
-	'The Book of Job',
-	'The Book of Numbers',
-	'The Book of Deuteronomy',
-	'The Book of Revelations',
-	'The Tibetan Book of the Dead',
-	"Edward Gibbon's The History of the Decline and Fall of the Roman Empire Book 3 and 4",
-	'The Poetic Edda',
-	"Herodotus's The Histories",
-	'Secret History of the Mongols',
-	"Ovid's Metamorphoses",
-	"Virgil's Aeneid",
-	'The Epic of Gilgamesh',
-	'The Mahabharata',
-	"Sima Qian's Records of the Grand Historian",
-	'Beowulf',
-	"Homer's Odyssey",
-	"Homer's Iliad",
+	'The Old Regime and the Revolution by Alexis de Tocqueville',
+	'Petersburg by Andrei Bely',
+	'Lives of the Most Excellent Painters, Sculptors, and Architects by Giorgio Vasari',
+	'Parallel Lives by Plutarch',
+	'The Nibelungenlied',
+	'King Arthur and His Knights of the Round Table',
+	'Canebury Tales by Geoffrey Chaucer',
 ];
 
 enum Medium {
@@ -57,7 +67,7 @@ const getArtPrompt =
 	(openAIApiKey: string) =>
 	(expertise: string): TaskEither<any, string> =>
 		sendPrompt(openAIApiKey)(`You are a history expert who comes up with historic art prompt from a part of ${expertise}.
-		The prompt should be able to fit in a tweet. Please include the year in parentheses at the end of the prompt.
+		The prompt should be able to fit in a tweet, under 280 characters. Please include the year in parentheses at the end of the prompt.
 		You respond with the prompt and the prompt only.
 		Here are some examples. Your prompt should be similar to structure, content, and length as the following examples:
 
@@ -78,10 +88,35 @@ const modernizeArtPrompt =
 			openAIApiKey
 		)(`You are an artist that creates art that juxtaposes a moment in ancient history over a backdrop of a modern landscape composed of symbols, cultural references, memes, and pop culture.
 		More specifically, you take the below paragraph which describes a historic moment along with a paragraph that summarizes current events provided by the end user.
-		You then merge the two together to producing a paragraph describing the moment in ancient history but with modern embellishments.
-		Ensure your cultural references are blatant, hyper modern, pop-art adjacent. Try to include a slice of pizza, bowl of buffalo wings, american football, guitars, sunglasses, expensive handbags, military grade weapons, or something hyper American:
+		You then merge the two together to produce a paragraph describing the moment in ancient history but with modern embellishments.
+		Ensure your cultural references are blatant, hyper modern, pop-art adjacent.
+		Try to include a slice of pizza, bowl of buffalo wings, american football, guitars, sunglasses, potato chips, microsoft computers, expensive handbags, military grade weapons, large cartoony shoes like Big Red Shoes and Balenciaga, the cereal Fruit Loops, american cash money, grubhub and seamless delivery cyclists who wear face coverings, amazon prime delivery trucks and drivers, or something hyper American.
+		Also try to include references to Gen Z fashion, such as baggy pants, camo, low rising jeans, and a liking for 90s and 2000s pop culture like grunge and Nu Meta music.
+		Also try to throw in a reference to the video game series Metal Gear Solid and the game Death Stranding.
+		Please give the historic figure represented in the prompt a likeness with a famous person mentioned in the modern news summary.
 
+The news paragraph:
 ${history}`)(news);
+
+const addHashTags =
+	(openAIApiKey: string) =>
+	(context: string) =>
+	(tweetText: string): TaskEither<any, string> =>
+		sendPrompt(openAIApiKey)(
+			`You are responsible for adding hash tags to a tweet based on the below provided context.
+			More specifically, the hashtags should describe the moment in history but also have tags which reference the provided context of current events.
+			Please return just the provided tweet with hash tags at the end. Do not modify the actual provided text.
+
+			Here is the context:
+${context}`
+		)(tweetText);
+
+const summarizeArtPrompt =
+	(openAIApiKey: string) =>
+	(prompt: string): TaskEither<any, string> =>
+		sendPrompt(openAIApiKey)(
+			`You are responsible for taking a prompt provided by an artist and simplifying it into a prompt for DALL-E image generation`
+		)(prompt);
 
 const sendPrompt =
 	(openAIApiKey: string) =>
@@ -123,7 +158,7 @@ const randomMember = (list: string[]): any => {
 };
 
 export default {
-	async fetch(event: any, env: Env, ctx): Promise<Response> {
+	async scheduled(event: any, env: Env, ctx): Promise<Response> {
 		const OPENAI_API_KEY: any = env.OPENAI_API_KEY;
 		const consumerKey: any = env.TWITTER_CONSUMER_KEY;
 		const consumerSecret: any = env.TWITTER_CONSUMER_SECRET;
@@ -151,9 +186,19 @@ export default {
 					fetchGoogleNewsHeadlines(),
 					te.chain(summarizeNews(OPENAI_API_KEY)),
 					te.chain(modernizeArtPrompt(OPENAI_API_KEY)(caption)),
-					te.map(buildArtPrompt(randomMedium())),
-					te.chain(genImage(OPENAI_API_KEY)),
-					te.chain(sendTweet(twitter)(caption))
+					te.chain(summarizeArtPrompt(OPENAI_API_KEY)),
+					te.chain((artPrompt) =>
+						pipe(
+							te.of(buildArtPrompt(randomMedium())(artPrompt)),
+							te.chain(genImage(OPENAI_API_KEY)),
+							te.chain((imgUrl) =>
+								pipe(
+									addHashTags(OPENAI_API_KEY)(artPrompt)(caption),
+									te.chain((tweetText) => sendTweet(twitter)(tweetText)(imgUrl))
+								)
+							)
+						)
+					)
 				)
 			),
 			te.fold(
